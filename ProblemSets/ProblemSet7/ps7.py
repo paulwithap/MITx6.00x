b66f297f728655c4ffca886021f3f9934aa02b9a
paulwithap <pjaworski27@gmail.com>
2013-04-14 14:51:49 -0700

    build RandomWalkRobot class

--- ProblemSets/ProblemSet7/ps7.py
+++ ProblemSets/ProblemSet7/ps7.py
@@ -289,7 +289,7 @@ def runSimulation(num_robots, speed, width, height, min_coverage, num_trials,
 
         #anim.done()
 
-    meanTime = float(sum(timeList)) / float(len(timeList))
+    meanTime = sum(timeList) / len(timeList)
 
     return meanTime
 
@@ -307,7 +307,15 @@ class RandomWalkRobot(Robot):
         Move the robot to a new position and mark the tile it is on as having
         been cleaned.
         """
-        raise NotImplementedError
+        self.setRobotDirection(random.randint(0, 360))
+        newPos = self.position.getNewPosition(self.direction, self.speed)
+
+        while not self.room.isPositionInRoom(newPos):
+            self.setRobotDirection(random.randint(0, 360))
+            newPos = self.position.getNewPosition(self.direction, self.speed)
+
+        self.position = newPos
+        self.room.cleanTileAtPosition(self.position)
 
 
 def showPlot1(title, x_label, y_label):
